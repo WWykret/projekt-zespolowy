57593ad0013a6ed76b6741206de0609f205a902c
WWykret <57493639+WWykret@users.noreply.github.com>
2022-05-14 13:43:26 +0200

    Added possibility to send graphs and predictions (#6)

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     last_scan_date = utils.get_last_scan_date()
 
     for index, stock in avilable_stocks.iterrows():
-        if not predictor.has_training_data(stock['Symbol']) or (last_scan_date < today):
-            stock_symbol = stock['Symbol']
+        if not predictor.has_training_data(stock['Code']) or (last_scan_date < today):
+            stock_symbol = stock['Code']
             print(f'{index + 1} / {avilable_stocks.shape[0]}... {stock_symbol}')
             stock_data = ds.dataScraper(stock_symbol)
             predictor.save_training_data(stock_symbol, stock_data)
